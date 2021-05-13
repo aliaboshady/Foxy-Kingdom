7a2313415b3679f065c156867ae14fa41951e8ce
Ali Aboshady <ali_aboshady@hotmail.com>
2021-05-14 00:37:24 +0100

    Player can jump and face movement direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 20f;
 
     Rigidbody2D rigidBody;
-    float horAxis;
+    bool canJump = true;
+    int facingDirection;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        facingDirection = transform.localScale.x < 0 ? -1 : 1;
     }
 
-    void Update()
-    {
-        GetMoveInput();
-    }
-
-	void FixedUpdate()
+	void Update()
 	{
+        FaceRightDirection();
         Move();
+        if(canJump && Input.GetKeyDown(KeyCode.Space))
+		{
+            Jump();
+		}
     }
 
-	void GetMoveInput()
-	{
-        horAxis = Input.GetAxis("Horizontal") * moveSpeed;
-    }
     void Move()
 	{
+        float horAxis = Input.GetAxis("Horizontal") * moveSpeed;
+        if(horAxis > 0)
+		{
+            facingDirection = 1;
+		}
+        else if(horAxis < 0)
+		{
+            facingDirection = -1;
+		}
         rigidBody.velocity = new Vector2(horAxis, rigidBody.velocity.y);
+	}
+
+    void Jump()
+	{
+        rigidBody.velocity += Vector2.up * jumpSpeed;
+	}
+
+    void FaceRightDirection()
+	{
+        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * facingDirection, transform.localScale.y, transform.localScale.z);
 	}
 }
